fe2bb44619fcfa54d6625f2cb1a193d41b04eda2
Jerson Júnior <jersonjunior@bsd.com.br>
2024-11-21 13:55:10 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-chmod 777 /freqtrade/user_data/logs/freqtrade.log
-exec "$@"
+
+# Garante que o arquivo de log existe
+if [ ! -f /freqtrade/user_data/logs/freqtrade.log ]; then
+  touch /freqtrade/user_data/logs/freqtrade.log
+fi
+
+# Aplica permissões somente se possível
+chmod 777 /freqtrade/user_data/logs/freqtrade.log || echo "Aviso: não foi possível alterar permissões"
+
+# Executa o comando principal
+exec freqtrade "$@"
